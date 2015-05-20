a7977e9c3596d6a45011389672449d7cc48c8128
Anant Gupta <anantgupta@google.com>
2015-05-20 13:24:50 -0700

    asf

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function jsonFlickrApi(response) {
-	var cats = $("#cats");
+	/*var cats = $("#cats");
 	$.each(response.photos.photo, function(i, item) {
 		var src = "https://farm"
 			+ item.farm
@@ -11,9 +11,56 @@ function jsonFlickrApi(response) {
 		var container = $("<li/>").appendTo(cats);
 		$("<img/>").attr("src", src).appendTo(container);
 	});
+	*/
+	
+	var cats = document.getElementById("cat");
+	$.each(response.photos.photo, function(i, item) {
+		var src = "https://farm"
+			+ item.farm
+			+ ".static.flickr.com/"
+			+ item.server
+			+ "/" + item.id
+			+ "_" + item.secret
+			+ "_n.jpg";
+		var container = document.createElement('LI');
+		var image = document.createElement('img');
+		image.src = src;
+		container.appendChild(image);
+		cats.appendChild(container);
+	});
 }
 
 $(function() {
+
+  /*var init = { method: 'GET' };    
+  fetch(imgJSON.url).then(function(response) {
+    if (response.status == 200) {
+      var arrayResponse = [];
+      response.blob().then(function(myBlob) {
+        arrayResponse[0] = myBlob;
+        arrayResponse[1] = imgJSON;
+        var myImage = document.createElement('img');
+        var myFigure = document.createElement('figure');
+        var myCaption = document.createElement('caption');
+        var imageURL = window.URL.createObjectURL(arrayResponse[0]);
+
+        myImage.src = imageURL;
+        myImage.setAttribute('alt', arrayResponse[1].alt);
+        myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
+        
+        var imgSection = document.querySelector('section');
+        imgSection.appendChild(myFigure);
+        myFigure.appendChild(myImage);
+        myFigure.appendChild(myCaption);
+      });
+
+    } else {
+      reject(Error('Image didn\'t load successfully; error code:' + response.statusText));
+    }
+  }, function() {
+    reject(Error('There was a network error.'));
+  });*/
+
 	$.ajax({
 		url: flickr_api_url,
 		method: 'GET',
